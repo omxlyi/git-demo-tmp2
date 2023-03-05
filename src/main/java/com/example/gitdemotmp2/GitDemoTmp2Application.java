a7c7cf348b3e6e9ab238354652a3c2faf1fbe440
Lacia <weiyinsama@gmail.com>
2023-03-05 18:08:57 +0800

    第二次提交

--- src/main/java/com/example/gitdemotmp2/GitDemoTmp2Application.java
+++ src/main/java/com/example/gitdemotmp2/GitDemoTmp2Application.java
@@ -8,6 +8,8 @@ public class GitDemoTmp2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(GitDemoTmp2Application.class, args);
+
+        System.out.println("第二次修改");
     }
 
 }
