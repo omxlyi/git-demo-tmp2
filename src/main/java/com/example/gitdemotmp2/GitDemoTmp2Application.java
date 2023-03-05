a4d0c27c01e92100b5557cffc478e3a35ac1af37
Lacia <weiyinsama@gmail.com>
2023-03-05 18:04:45 +0800

    haha!

---
+++ src/main/java/com/example/gitdemotmp2/GitDemoTmp2Application.java
@@ -0,0 +1,13 @@
+package com.example.gitdemotmp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoTmp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemoTmp2Application.class, args);
+    }
+
+}
